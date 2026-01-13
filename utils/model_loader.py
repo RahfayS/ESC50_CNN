ec81f6275e5587df0156d24eb1e55fd4a2e8fe8f
Rahfay <saberirahfay@gmail.com>
2026-01-13 09:21:50 -0500

    Refactor model loader to use hugging face

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,9 +1,10 @@
 import torch
-import torch.nn as nn
-from torchvision import models
 from model import ResNet34
+from huggingface_hub import hf_hub_download
+import streamlit as st
 
-def load_model(pth_path, device = None):
+@st.cache_resource(show_spinner="Loading CNN models...")
+def load_model(device = None):
     """
     Loads a from scratch ResNet34
     
@@ -12,16 +13,24 @@ def load_model(pth_path, device = None):
         device (torch.device): CPU or GPU. If None, auto-detect.
     
     Returns:
-        model (nn.Module): Ready-to-use model
+        models (nn.Module): Ready-to-use model
     """
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    resNet = ResNet34() # From Scratch Model
-    resNet.load_state_dict(
-        torch.load(pth_path, map_location=device)
+    resNet_balanced = ResNet34() # From Scratch Model
+    resNet_balanced_path = hf_hub_download(
+        repo_id="Rahfay/ESC-50-CNN",
+        filename="ResNet34_balanced_augmentation.pth"
     )
-    resNet.to(device)
-    print(f"Model Loaded")
-    resNet.eval()
-    return resNet
+
+    resNet_acoustic = ResNet34() # From Scratch Model
+    resNet_acoustic_path = hf_hub_download(
+        repo_id="Rahfay/ESC-50-CNN",
+        filename="ResNet34_acoustic_augmentation.pth"
+    )
+
+    resNet_balanced.load_state_dict(torch.load(resNet_balanced_path,map_location=device))
+    resNet_acoustic.load_state_dict(torch.load(resNet_acoustic_path,map_location=device))
+
+    return resNet_balanced, resNet_acoustic
